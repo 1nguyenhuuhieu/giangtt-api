001b25750d2502db967eea00984f08d62db73333
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-07-19 20:27:37 +0700

    b

--- main.py
+++ main.py
@@ -1,60 +1,30 @@
-import requests
 from pydantic import BaseModel
-import base64
-import hashlib
-import hmac
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
+from tarzapay import *
+
+
+tarzapay_api_Key = 'GSLL9GDA84URSS7TSA2Z'
+tarzapay_secret = 'sandbox_gPIMe0IIIxd7x3HHVBpUPki32eNV8AC84lByYTNaD7JDgGpIMZRZa4dVUmFlY0M8otDUyAxBw8AoSLObmkvZEtL5Aq70U7IPAgKddMy7bU7vIx4SWokkcVfI9CI4pWXB'
 
 app = FastAPI()
 
-api_url = "https://api-sandbox.tazapay.com"
-api_key = 'GSLL9GDA84URSS7TSA2Z'
-api_secret = 'sandbox_gPIMe0IIIxd7x3HHVBpUPki32eNV8AC84lByYTNaD7JDgGpIMZRZa4dVUmFlY0M8otDUyAxBw8AoSLObmkvZEtL5Aq70U7IPAgKddMy7bU7vIx4SWokkcVfI9CI4pWXB'
+class Buyer(BaseModel):
+    ind_bus_type: str = Field("Individual")
+    email: str
+    country: str
+    first_name: str
+    last_name: str
 
-def generate_signature(api_secret, data):
-    key_bytes = bytes(api_secret, 'latin-1')
-    data_bytes = bytes(data, 'latin-1')
-    signature = hmac.new(key_bytes, msg=data_bytes, digestmod=hashlib.sha256).digest()
-    return base64.b64encode(signature).decode('latin-1')
+class Transaction(BaseModel):
+    buyer: Buyer
+    invoice_currency: str = Field("USD")
+    invoice_amount: float
+    txn_description: str
 
-def send_request(endpoint, method, api_url, api_key, api_secret, data=None):
-    url = api_url + endpoint
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Basic {base64.b64encode(f"{api_key}:".encode()).decode()}'
-    }
-    if data:
-        signature = generate_signature(api_secret, data)
-        headers['Signature'] = signature
-
-    response = requests.request(method, url, headers=headers, json=data)
-
-    if response.status_code != 200:
-        raise HTTPException(status_code=response.status_code, detail=response.text)
-
-    return response.json()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/payment")
-def make_payment(amount: float, user_id: int):
-    endpoint = '/v1/payment'  # Địa chỉ API cụ thể trong Tarzapay
-
-    data = {
-        'amount': amount,
-        'user_id': user_id
-    }
-
-    try:
-        response = send_request(endpoint, 'POST', api_url, api_key, api_secret, data)
-        return response
-    except HTTPException as e:
-        raise HTTPException(status_code=e.status_code, detail=e.detail)
+@app.post("/transactions/")
+async def create_transaction(transaction: Transaction):
+    # Process the logic to create a transaction and save it to the database
+    # For example: Print transaction information and return a response
+    return {"message": "Transaction created successfully", "transaction": transaction}
--- tarzapay.py
+++ tarzapay.py
@@ -1,12 +1,6 @@
 import requests
 import base64
 
-API_url = 'https://api-sandbox.tazapay.com/v1/checkout'
-API_Key = 'GSLL9GDA84URSS7TSA2Z'
-API_Secret = 'sandbox_gPIMe0IIIxd7x3HHVBpUPki32eNV8AC84lByYTNaD7JDgGpIMZRZa4dVUmFlY0M8otDUyAxBw8AoSLObmkvZEtL5Aq70U7IPAgKddMy7bU7vIx4SWokkcVfI9CI4pWXB'
-
-
-
 # Tạo header Authorization với thông tin xác thực Basic
 def create_basic_auth_header(username, password):
     auth_string = f"{username}:{password}"
@@ -15,31 +9,15 @@ def create_basic_auth_header(username, password):
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "authorization": f"Basic {base64_auth_string}"
-        }
+        "authorization": f"Basic {base64_auth_string}"}
     return headers
 
+# Tạo yêu cầu thanh toán
+def create_checkout_session(payload, username, password):
 
-# Tạo yêu cầu sử dụng phương thức Basic Authentication
-def make_authenticated_request(url, username, password):
-    payload = {
-        "buyer": {
-            "ind_bus_type": "Individual",
-            "email": "a@string.com",
-            "country": "SG",
-            "first_name": "sg",
-            "last_name": "buyer"
-        },
-        "invoice_currency": "USD",
-        "invoice_amount": 100,
-        "txn_description": "test"
-    }
+    print(payload)
+
+    url = "https://api-sandbox.tazapay.com/v1/checkout"
     headers = create_basic_auth_header(username, password)
-    response = requests.get(url, json=payload, headers=headers)
+    response = requests.post(url, json=payload, headers=headers)
     return response
-
-
-# Thực hiện yêu cầu với thông tin xác thực Basic
-response = make_authenticated_request(API_url, API_Key, API_Secret)
-
-print(response.text)
